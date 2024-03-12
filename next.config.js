41a2990ebec5bf6af6b668e72681ccaa5c1509af
JakeerC <jakeerchilakala@gmail.com>
2024-03-12 10:10:50 +0530

    revert: allow cors

--- next.config.js
+++ next.config.js
@@ -4,25 +4,4 @@ module.exports = {
   eslint: {
     dirs: ['src'],
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 };
